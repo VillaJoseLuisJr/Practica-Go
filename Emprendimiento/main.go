69a10e0ea21f1a3220ce8504ecae73f7414a2d3b
VillaJoseLuisJr <villa.joseluisjr@gmail.com>
2025-04-15 14:53:40 -0300

    Ahora se puede subir un producto con nombre y descripción desde el formulario y se muestra en el index.html. Arreglé el error que daba al no subir imágen.

--- Emprendimiento/main.go
+++ Emprendimiento/main.go
@@ -10,14 +10,45 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+type Producto struct {
+	ID          int
+	Nombre      string
+	Descripcion string
+	Imagen      sql.NullString
+}
+
+var templates = template.Must(template.ParseFiles("templates/index.html"))
+
 // Funcion para mostrar el HTML
 func mostrarProductos(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("templates/index.html")
+	// Obtener los productos de la base de datos
+	filas, err := db.Query("SELECT id, nombre, descripcion, imagen FROM productos")
 	if err != nil {
-		http.Error(w, "Error al cargar el template", http.StatusInternalServerError)
+		log.Println("Error al hacer la consulta", err)
+		http.Error(w, "Error al obtener productos", http.StatusInternalServerError)
 		return
 	}
-	tmpl.Execute(w, nil)
+	defer filas.Close()
+
+	// Crear un slice de productos
+	var productos []Producto
+
+	for filas.Next() {
+		var p Producto
+		err := filas.Scan(&p.ID, &p.Nombre, &p.Descripcion, &p.Imagen)
+		if err != nil {
+			log.Println("Error al renderizar template", err)
+			http.Error(w, "Error al leer productos", http.StatusInternalServerError)
+			return
+		}
+		productos = append(productos, p)
+	}
+
+	// Renderizar el template
+	if err := templates.Execute(w, productos); err != nil {
+		http.Error(w, "Error al renderizar plantilla", http.StatusInternalServerError)
+	}
+	//templates.Execute(w, productos)
 }
 
 // Para conectar con la base de datos
@@ -25,7 +56,7 @@ var db *sql.DB
 
 func init() {
 	var err error
-	dsn := "root:@tcp(localhost:3306)/emprendimiento"
+	dsn := "root:@tcp(localhost:3306)/emprendimiento_go"
 	db, err = sql.Open("mysql", dsn)
 	if err != nil {
 		log.Fatal(err)
@@ -39,12 +70,34 @@ func init() {
 	log.Println("Conexión a MySQL exitosa")
 }
 
-func mostrarFormulario() {
-
+// Función para mostrar el formulario de agregar producto
+func mostrarFormulario(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("templates/formulario.html")
+	if err != nil {
+		http.Error(w, "Error cargando template", http.StatusInternalServerError)
+		return
+	}
+	tmpl.Execute(w, nil)
 }
 
-func guardarProducto() {
+// Función para guardar el producto en la base de datos
+func guardarProducto(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		// Obtener datos del formulario
+		Nombre := r.FormValue("nombre")
+		Descripcion := r.FormValue("descripcion")
+		// Código para manejar la imagen, lo dejamos para después
 
+		// Guardar el producto en la base de datos
+		_, err := db.Exec("INSERT INTO productos (nombre, descripcion) VALUES (?, ?)", Nombre, Descripcion)
+		if err != nil {
+			http.Error(w, "Error al guardar el producto", http.StatusInternalServerError)
+			return
+		}
+
+		//Redirigir a la página de productos
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	}
 }
 
 func main() {
@@ -52,8 +105,10 @@ func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.Handle("/imagenes/", http.StripPrefix("/imagenes/", http.FileServer(http.Dir("imagenes"))))
 
-	// Ruta principal
+	// Rutas
 	http.HandleFunc("/", mostrarProductos)
+	http.HandleFunc("/formulario", mostrarFormulario)
+	http.HandleFunc("/guardar", guardarProducto)
 
 	// Inicia el servidor en puerto 8080
 	log.Println("Servidor iniciado en http://localhost:8080")
