129769691403b299797a454824ad285ca2ae469e
Junior <villa.joseluisjr@gmail.com>
2025-07-22 14:43:35 -0300

    Llegué al minuto 20:38 del video

--- go-bookstore/pkg/routes/bookstore-routes.go
+++ go-bookstore/pkg/routes/bookstore-routes.go
@@ -1,6 +1,12 @@
 package routes
 
-//"github.com/gorilla/mux"
-//"github.com/VillaJoseLuisJr/Practica-Go/tree/tutoriales-youtube/go-bookstore/pkg/controllers"
+"github.com/gorilla/mux"
+"github.com/VillaJoseLuisJr/Practica-Go/tree/tutoriales-youtube/go-bookstore/pkg/controllers"
 
-var RegisterBookStoreRoutes = func()
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+	router.HandlerFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandlerFunc("/book/", controllers.GetBook).Methods("GET")
+	router.HandlerFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandlerFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandlerFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
+}
